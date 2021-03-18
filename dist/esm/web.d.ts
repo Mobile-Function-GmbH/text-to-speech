10fdb91276bdd29bbf8575f8c66347f20ffbb8de
Robin Genz <mail@robingenz.dev>
2021-03-18 09:15:05 +0100

    publish dist folder

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,32 @@
+import { WebPlugin } from '@capacitor/core';
+import { TextToSpeechPlugin, SpeechSynthesisVoice, TTSOptions } from './definitions';
+export declare class TextToSpeechWeb extends WebPlugin implements TextToSpeechPlugin {
+    private speechSynthesis;
+    constructor();
+    speak(options: TTSOptions): Promise<void>;
+    stop(): Promise<void>;
+    getSupportedLanguages(): Promise<{
+        languages: string[];
+    }>;
+    getSupportedVoices(): Promise<{
+        voices: SpeechSynthesisVoice[];
+    }>;
+    openInstall(): Promise<void>;
+    setPitchRate(_options: {
+        pitchRate: number;
+    }): Promise<void>;
+    setSpeechRate(_options: {
+        speechRate: number;
+    }): Promise<void>;
+    isLanguageSupported(options: {
+        lang: string;
+    }): Promise<{
+        supported: boolean;
+    }>;
+    private createSpeechSynthesisUtterance;
+    private getSpeechSynthesisVoices;
+    private throwUnsupportedError;
+    private throwUnimplementedError;
+}
+declare const TextToSpeech: TextToSpeechWeb;
+export { TextToSpeech };
